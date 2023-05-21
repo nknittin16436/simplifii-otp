578bfc6cd88c7edd19a61773153192dc362ba9f3
Nand <nknittin16436@gmail.com>
2023-05-21 10:43:18 +0530

    added node engine in package.json

--- dist/app.js
+++ dist/app.js
@@ -9,5 +9,8 @@ const login_router_1 = require("./routes/login.router");
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(body_parser_1.default.json());
+app.get('/', (req, res) => {
+    res.send('Hey this is my OTP API running 🥳');
+});
 app.use("/api/v1", login_router_1.LoginRouter);
 exports.default = app;
